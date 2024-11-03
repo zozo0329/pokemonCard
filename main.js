1fbb4344504f4d7675f003c68552ee259508b663
Zo <161946731+zozo0329@users.noreply.github.com>
2024-11-03 21:41:57 +0800

    all fixed

--- main.js
+++ main.js
@@ -6,11 +6,8 @@ async function pokemonFunction() {
     if (!res.ok) {
       throw new Error("Error, Could not fetch resource");
     }
-    //
     const data = await res.json();
     console.log(data);
-    //
-    //
     const health = data.stats[0].base_stat;
     const sprite = data.sprites.other.dream_world.front_default;
     const pName = data.name;
@@ -22,7 +19,6 @@ async function pokemonFunction() {
     nameOfPokemon.innerHTML = capitalizedName;
     pokemonHp.innerHTML = "Hp:" + health;
     pokemonIMG.src = sprite;
-
     // stats
     const atkStat = data.stats[1].base_stat;
     const attack = document.querySelector(".atk");
@@ -38,37 +34,34 @@ async function pokemonFunction() {
 
     // Other functions
     appendTypes(data.types);
+    changeCardBackground(data.types);
   } catch (error) {
     console.log(error);
   }
 }
+
 function appendTypes(types) {
-  types.forEach((item) => {
-    const para = document.createElement("p");
-    para.textContent = item.type.name;
-    document.querySelector(".type").appendChild(para);
-  });
-}
-function appendTypes(types) {
+  document.querySelector(".type").innerHTML = "";
+
   const typeColors = {
-    normal: "#A8A77A",
-    fire: "#EE8130",
-    water: "#6390F0",
-    electric: "#F7D02C",
-    grass: "#7AC74C",
-    ice: "#96D9D6",
-    fighting: "#C22E28",
-    poison: "#A33EA1",
-    ground: "#E2BF65",
-    flying: "#A98FF3",
-    psychic: "#F95587",
-    bug: "#A6B91A",
-    rock: "#B6A136",
-    ghost: "#735797",
-    dragon: "#6F35FC",
-    dark: "#705746",
-    steel: "#B7B7CE",
-    fairy: "#D685AD",
+    normal: "#8A7A5B",
+    fire: "#CB6A28",
+    water: "#4A72B0",
+    electric: "#D0B921",
+    grass: "#65903C",
+    ice: "#7CABAE",
+    fighting: "#A0221D",
+    poison: "#852E82",
+    ground: "#BB9A52",
+    flying: "#8873C3",
+    psychic: "#D23F6D",
+    bug: "#838217",
+    rock: "#947C2B",
+    ghost: "#594477",
+    dragon: "#5530CB",
+    dark: "#574436",
+    steel: "#9393A3",
+    fairy: "#B0698D",
   };
 
   types.forEach((item) => {
@@ -80,6 +73,37 @@ function appendTypes(types) {
     para.style.backgroundColor = typeColors[typeName];
     document.querySelector(".type").appendChild(para);
   });
+}
+
+function changeCardBackground(types) {
+  const card = document.querySelector(".card");
+  const typeColors = {
+    normal: ["#A8A77A", "#C6C6A7"],
+    fire: ["#EE8130", "#FF9C54"],
+    water: ["#6390F0", "#86BFFF"],
+    electric: ["#F7D02C", "#FBE273"],
+    grass: ["#7AC74C", "#A7DB8D"],
+    ice: ["#96D9D6", "#BCEEF4"],
+    fighting: ["#C22E28", "#E1564D"],
+    poison: ["#A33EA1", "#C869C7"],
+    ground: ["#E2BF65", "#F1DCA3"],
+    flying: ["#A98FF3", "#C6B7F5"],
+    psychic: ["#F95587", "#FF82AB"],
+    bug: ["#A6B91A", "#C6D16E"],
+    rock: ["#B6A136", "#D1C17D"],
+    ghost: ["#735797", "#A27DFA"],
+    dragon: ["#6F35FC", "#8571E4"],
+    dark: ["#705746", "#A29288"],
+    steel: ["#B7B7CE", "#D1D1E0"],
+    fairy: ["#D685AD", "#EBA7C6"],
+  };
+  if (types.length > 0) {
+    const typeName = types[0].type.name.toLowerCase();
+    const colors = typeColors[typeName];
+    if (colors) {
+      card.style.background = `linear-gradient(to right, ${colors[0]}, ${colors[1]})`;
+    }
+  }
 }
 
 /* 
